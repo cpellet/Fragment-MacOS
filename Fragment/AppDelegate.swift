933fce47913672117ca804d0288c3ce9ac79d0da
Cyrus <cplt@pm.me>
2020-08-22 13:43:49 +0200

    Initial Commit

---
+++ Fragment/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fragment
+//
+//  Created by Cyrus Pellet on 22/08/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
